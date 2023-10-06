f760332e0fc197e6e22438b60ef750654681f0c6
Melissa Johansson <mejo22@student.bth.se>
2023-10-06 11:03:33 +0200

    Add custom link and column in table

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { config } from './Constants';
+
+const URL = config.url;
 
 export default function Table() {
     // get data on trains for table
-    const url = "https://jsramverk-trains-meda23.azurewebsites.net/delayed";
+    // const url = "https://jsramverk-trains-meda23.azurewebsites.net/delayed";
+    const url = `${URL}/delayed`
 
     const [result, setData] = useState([]);
 
@@ -26,6 +30,7 @@ export default function Table() {
                             <th>Current station:</th>
                             <th>Route:</th>
                             <th>Delay:</th>
+                            <th>Add ticket:</th>
                         </tr>
                         {result.map((data, index) => {
                             return (
@@ -34,6 +39,7 @@ export default function Table() {
                                     <td style={{width: "23vh"}}>{data.LocationSignature}</td>
                                     <td style={{width: "23vh"}}>{data.FromLocation ? data.FromLocation[0].LocationName + " -> " : ""} {data.ToLocation ? data.ToLocation[0].LocationName : ""}</td>
                                     <td style={{width: "20vh"}}>{outputDelay(data)}</td>
+                                    <td style={{width: "20vh"}}><Link to="/ticket" state={{ data: data }} className="link">+</Link></td>
                                 </tr>
                             );
                         })}
